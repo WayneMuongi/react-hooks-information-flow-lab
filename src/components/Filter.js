d2ba60d1c0e6f2ba743e22ea32919539c7d789f2
WayneMuongi <waynemuongi77@gmail.com>
2025-07-29 18:12:33 +0300

    woah 3!

---
+++ src/components/Filter.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Filter({onCategoryChange, selectedCategory}){
+    return(
+
+        <select name="filter" onChange={onCategoryChange} value = {selectedCategory}>
+          <option value="All">Filter by category</option>
+          <option value="Produce">Produce</option>
+          <option value="Dairy">Dairy</option>
+          <option value="Dessert">Dessert</option>
+        </select>
+    )
+}
+
+export default Filter;
